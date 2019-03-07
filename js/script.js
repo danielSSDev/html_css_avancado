b4c9e951276162fc6600b862c2d2055bbc1ee729
Daniel <daniel-santos2000@live.com>
2019-03-07 14:07:54 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    //abre e fecha menu
+    $('.nav-toggle, .nav-close').click(function(e) {
+        e.preventDefault();
+        $('.nav').toggleClass('active');
+    });
+
+});
